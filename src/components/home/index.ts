dbdf2be301140a8e293a35239050f0c71fdc533e
Lamine Diaby <diaby.lamine@gmail.com>
2026-03-21 01:39:41 +0100

    refactor(home): update barrel exports for dashboard components

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -1,7 +1,10 @@
-export { DashboardStats } from "./DashboardStats";
-export { ActivityTabs } from "./ActivityTabs";
-export { QuickTools } from "./QuickTools";
-export { UpcomingElections } from "./UpcomingElections";
+export { WelcomeBar } from "./WelcomeBar";
+export { KPIStrip } from "./KPIStrip";
 export { ElectionBanner } from "./ElectionBanner";
-export { TodayVotesWidget } from "./TodayVotesWidget";
+export { TopMovers } from "./TopMovers";
+export { ActivityFeed } from "./ActivityFeed";
+export { QuickAccess } from "./QuickAccess";
+export { SupportBar } from "./SupportBar";
+
+// Legacy exports - kept for any remaining consumers
 export { HomeRecapSection } from "./HomeRecapSection";
